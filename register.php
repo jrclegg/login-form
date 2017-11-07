e13ba0c888c906d6cf1dcaadac3af4e3280caf02
jrclegg <clegg.jonathan@rocketmail.com>
2017-11-07 16:57:01 +0000

    model file removed, register file adjusted to include variables

--- register.php
+++ register.php
@@ -5,17 +5,23 @@
 
 <?php
 
-include 'model.php';
-// include variables from model.php file
+$mysql_connection = new mysqli("localhost", "root", "root", "login_system");
+ // Opens a new connection to the MySQL server, server = localhost, username = root, password = root, database = login_system
+ // $mysql_connection is the variable that represents the connection
 
 if ($mysql_connection -> connect_error) {
 	die("Connection failed: " . $mysql_connection -> connect_error);
 	//If database connection fails show an error message
 }
 
+
 if ($_POST) {
 	// Post is used to submit the user entered information to register an account
 	// If the form has been submitted process form data
+
+	$Email = $_POST['Email'];
+ 	$Password =$_POST['Password'];
+ 	// Store Email and Password user inputted data in variables
 
 	if (($Email) AND ($Password)) {
 		// Check to see if the user has provided an Email and a Password
@@ -25,6 +31,16 @@ if ($_POST) {
 			
 			if (password_strong($Password)) {
 				// Use the function "password_strong" to evaluate if the password matches strength parameters
+
+				// Create a unique activation code as we will put it in the database AND put it in the email
+				$code = rand(5000, 200);
+
+				// create an INSERT SQL query that populates the login_system rows with the user entered values stored inside the variables
+				$sql = "INSERT INTO login_system (email, password, activation_code) VALUES ('$Email', '$Password', '$code');";
+
+				// performs a query against the MySQL database
+				$result = mysqli_query($mysql_connection, $sql);
+
 
 				if ($result) {
 					//Check that the result ran successfully
